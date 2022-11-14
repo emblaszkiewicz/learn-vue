94c3c6eae592fa45f88253f68c0cb378db1c3dab
Michal <michal.blaszkiewicz@britenet.com.pl>
2022-11-14 16:05:28 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+//import { RouterLink, RouterView } from "vue-router";
+import Books from "./views/Books/Books-List.vue";
+import { ref } from "vue";
+const visability = ref(false);
+const toggle = (): void => {
+  visability.value = !visability.value;
+};
+</script>
+
+<template>
+  <h1>Vue learn!</h1>
+  <button @click="toggle">Show books!</button>
+  <Books v-if="visability" />
+</template>
+
+<style scoped></style>
